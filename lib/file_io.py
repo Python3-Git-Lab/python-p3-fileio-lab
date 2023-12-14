c48431febce93562df3f4e7a199641e8e84d1eb0
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-12-14 14:02:13 +0300

    Added reading, writing and append functions

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+    #"""Write the given content to a new file with the specified name.\
+    #If the file already exists, it will be overwritten.
+    #filename can be a combined file_path/name
+    #Example: "mydir/textfile.txt"
+    with open(f'{file_name}.txt' , mode= 'w', encoding='utf-8') as textfile:
+        textfile.write(file_content)
+write_file('logfile' , 'This is a test content')
 
 def append_file(file_name, append_content):
-    pass
+    #Append content to a file
+    #The file and the content are the parameters
+
+    with open(f'{file_name}.txt' , mode= 'a', encoding='utf-8') as logfile:
+        logfile.write(append_content)
+append_file("logfile" , "Log 2: 3 bananas subtracted")
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt' , mode= 'r', encoding='utf-8') as logfile:
+        return logfile.read()
